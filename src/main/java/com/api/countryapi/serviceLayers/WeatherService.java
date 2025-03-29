fa67c54aec047111ab2df96ceb029661bc5868d5
VinceQuiren <vincequiren2829@gmail.com>
2025-03-29 12:40:42 +0800

    Update WeatherService.java

--- src/main/java/com/api/countryapi/serviceLayers/WeatherService.java
+++ src/main/java/com/api/countryapi/serviceLayers/WeatherService.java
@@ -8,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
+    @Value("${weather.api.key}")
+    private String apiKey;
     private final RestTemplate restTemplate;
 
     public WeatherService(RestTemplateBuilder restTemplateBuilder){
@@ -18,7 +20,7 @@ public class WeatherService {
 
     public DataResponseWeathers getWeathers(@RequestParam String city){
 
-        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+"0f48147f9d993aae1b6e2a208d27b730";
+        String url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey;
         DataResponseWeathers dataResponseWeathers = restTemplate.getForObject(url, DataResponseWeathers.class);
         return dataResponseWeathers != null ? dataResponseWeathers: null;
     }
